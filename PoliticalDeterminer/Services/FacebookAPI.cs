c2bbf54a532d456b569ad2d3be498f6e69d8ff25
dsaiontz <david.saiontz@gmail.com>
2017-10-14 18:36:34 -0400

    Fixed getting pageID for Facebook
    PageID should be fixed now

--- PoliticalDeterminer/Services/FacebookAPI.cs
+++ PoliticalDeterminer/Services/FacebookAPI.cs
@@ -2,19 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
 using PoliticalDeterminer.Models;
 using System.Net;
 using System.Diagnostics;
 using System.IO;
 using Newtonsoft.Json;
+using System.Net.Http;
 
 namespace PoliticalDeterminer.Services
 {
     public class FacebookAPI
     {
-        public FacebookPost[] GetPosts(string pageID)
+        public async System.Threading.Tasks.Task<FacebookPost[]> GetPostsAsync(string url)
         {
+            int lastIndexOfSlash = url.LastIndexOf("/");
+
+            string pageName = url.Substring(lastIndexOfSlash, url.Length);
+
+            HttpWebRequest findingPageID = WebRequest.CreateHttp($"https://graph.facebook.com/{pageName}" +
+                $"access_token={Credentials.FacebookApiID}|{Credentials.FacebookSecret}");
+
+            WebResponse pageIDResponse = findingPageID.GetResponse();
+
+            Stream pageIDStream = pageIDResponse.GetResponseStream();
+
+            string json;
+
+            using (StreamReader sr = new StreamReader(pageIDStream))
+            {
+                json = sr.ReadToEnd();
+            }
+
+            int y = json.IndexOf("id");
+
+            FacebookPost[] fbID = FacebookPage.FromJson(json).Data;
+
+            string pageID = fbID[0].Id;
+
             HttpWebRequest request = WebRequest.CreateHttp($"https://graph.facebook.com/v2.10/{pageID}/posts?limit=100&" +
                 $"access_token={Credentials.FacebookApiID}|{Credentials.FacebookSecret}");
             request.Method = "GET";
@@ -27,8 +51,8 @@ namespace PoliticalDeterminer.Services
 
             using (StreamReader sr = new StreamReader(stream))
             {
-                string json = sr.ReadToEnd();
-                posts = FacebookPage.FromJson(json).Data;
+                string jsonData = sr.ReadToEnd();
+                posts = FacebookPage.FromJson(jsonData).Data;
             }
 
             return posts;
@@ -36,7 +60,7 @@ namespace PoliticalDeterminer.Services
 
         public string[] GetPostMessages(string pageID)
         {
-            FacebookPost[] posts = GetPosts(pageID);
+            FacebookPost[] posts = GetPostsAsync(pageID);
 
             string[] messages = new string[posts.Length];
 
