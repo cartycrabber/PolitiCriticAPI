7908fc336d19c3f0118a53bbe1823807d073052d
Will Carty <cartycrabber@gmail.com>
2017-10-14 10:37:53 -0400

    Replace default ValuesController with LearningController, install Accord.ML

---
+++ PoliticalDeterminer/Controllers/LeaningController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PoliticalDeterminer.Controllers
+{
+    public class LeaningController : ApiController
+    {
+        [Route("leaning/facebook/{id}")]
+        public int GetFacebookLeaning(string id)
+        {
+            return 0;
+        }
+
+        [Route("leaning/reddit/{id}")]
+        public int GetRedditLeaning(string id)
+        {
+            return 0;
+        }
+    }
+}
